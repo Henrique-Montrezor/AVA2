d8ea1b44f6757a3291872e1ce539f5601e33f41a
unknown <Rickmello737@gmail.com>
2026-03-20 17:06:27 -0300

    commit final

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,16 @@ const sessions = new Map();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 const hashPassword = (password, salt = null) => {
   const safeSalt = salt || crypto.randomBytes(16).toString('hex');
   const hash = crypto.pbkdf2Sync(password, safeSalt, 100000, 64, 'sha512').toString('hex');
